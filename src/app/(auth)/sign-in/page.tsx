9505459ee09c736bd641747beb026b16975fe0f9
Bruno Fabre <bruno.hfabre@gmail.com>
2025-11-19 14:07:26 -0300

    fix sign in flow

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,17 @@
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { auth } from '@/lib/auth'
 import { GithubButton } from './_components/github-button'
 
-export default function SignIn() {
+export default async function SignIn() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (session) {
+    return redirect('/')
+  }
+
   return (
     <div className="flex flex-col p-4 gap-4">
       <span>sign in</span>
