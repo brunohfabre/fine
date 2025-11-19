a5242e736fac580f9a4b7edd2e4f82bb3081336c
Bruno Fabre <bruno.hfabre@gmail.com>
2025-11-19 11:37:31 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+'use client'
+
+import { authClient } from '@/lib/auth-client'
+
 export default function Home() {
+  const { data: session } = authClient.useSession()
+
   return (
     <main>
       <div>Hello world!</div>
+
+      <button
+        type="button"
+        onClick={() =>
+          authClient.signIn.social({
+            provider: 'github',
+          })
+        }
+        className="px-4 h-10 bg-green-500"
+      >
+        sign in
+      </button>
+
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+
+      <button
+        type="button"
+        onClick={() => authClient.signOut()}
+        className="px-4 h-10 bg-red-500"
+      >
+        sign out
+      </button>
     </main>
-  );
+  )
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from '@/db/client'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    usePlural: true,
+  }),
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+})
